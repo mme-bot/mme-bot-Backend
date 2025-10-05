000372bd0d15b385608569f0bac6f9c168d7b434
h0l1da2 <holiday.k1@icloud.com>
2025-10-05 16:53:48 +0900

    🌸First Commit🌸
    hyuil

---
+++ src/main/java/me/mmebot/MmebotApplication.java
@@ -0,0 +1,13 @@
+package me.mmebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmebotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmebotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/mmebot/MmebotApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mmebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmebotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
