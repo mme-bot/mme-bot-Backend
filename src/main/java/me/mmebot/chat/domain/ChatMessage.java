589b3c0a8a78e9327af2ecc69288d485e330373a
h0l1da2 <holiday.k1@icloud.com>
2025-12-20 18:50:47 +0900

    chat Msg 에 replyMsg 컬럼 추가 (DB 변경)
    해당 메시지가 어떤 메시지의 답변 채팅인지 확인하기 위함
    
    hyuil

--- src/main/java/me/mmebot/chat/domain/ChatMessage.java
+++ src/main/java/me/mmebot/chat/domain/ChatMessage.java
@@ -45,9 +45,23 @@ public class ChatMessage {
     @JoinColumn(name = "encryption_context_id", nullable = false)
     private EncryptionContext encryptionContext;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reply_to_message_id")
+    private ChatMessage replyMsg;
+
     @CreationTimestamp
     @Column(name = "created_at", nullable = false, updatable = false)
     private OffsetDateTime createdAt;
+
+    public ChatMessage(ChatSession chatSession, Integer seq, ChatMessageRole role, String content, EncryptionContext encryptionContext, ChatMessage replyMsg) {
+        this.chatSession = chatSession;
+        this.seq = seq;
+        this.role = role;
+        this.content = content;
+        this.createdAt = OffsetDateTime.now();
+        this.encryptionContext = encryptionContext;
+        this.replyMsg = replyMsg;
+    }
 
     public ChatMessage(ChatSession chatSession, Integer seq, ChatMessageRole role, String content, EncryptionContext encryptionContext) {
         this.chatSession = chatSession;
@@ -56,5 +70,9 @@ public class ChatMessage {
         this.content = content;
         this.createdAt = OffsetDateTime.now();
         this.encryptionContext = encryptionContext;
+    }
+
+    public void updateReplyMsg(ChatMessage replyMsg) {
+        this.replyMsg = replyMsg;
     }
 }
